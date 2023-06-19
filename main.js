7b67c305839ad0a77fe63d46e11bb97dd6239fbb
Tashi Freyer <tashi.freyer@student.uibk.ac.at>
2023-06-19 16:13:47 +0200

    updated main.js from mtb side

--- main.js
+++ main.js
@@ -7,33 +7,39 @@ let ibk = {
 // Karte initialisieren
 let map = L.map("map", {
     fullscreenControl: true
-}).setView([ibk.lat, ibk.lng], 9);
+}).setView([ibk.lat, ibk.lng], 13);
 
 // thematische Layer
 let themaLayer = {
     routen: L.featureGroup(),
-    stops_bus: L.featureGroup(),
-    stops_tram: L.featureGroup(),
+    stops_bus: L.markerClusterGroup({ maxZoom: 22 }),
+    stops_tram: L.markerClusterGroup({ maxZoom: 22 }),
     huetten: L.featureGroup()
 }
 
 // WMTS und Leaflet TileLayerProvider Hintergrundlayer
-let layercontrol = L.control.layers({"Esri WorldTopoMap": L.tileLayer.provider("Esri.WorldTopoMap") }, {
+let layercontrol = L.control.layers({
+    "Esri WorldTopoMap": L.tileLayer.provider("Esri.WorldTopoMap"),
+    "BasemapAT Orthofoto": L.tileLayer.provider("BasemapAT.orthofoto").addTo(map)
+}, {
     "Mountainbikerouten": themaLayer.routen,
     "ÖPNV-Bus": themaLayer.stops_bus,
     "ÖPNV-Tram": themaLayer.stops_tram,
     "Hütten": themaLayer.huetten
 }).addTo(map)
 
-layerControl.expand()
+layercontrol.expand()
 
 // Maßstab
 L.control.scale({
     imperial: false,
 }).addTo(map);
 
+// Mini Map 
+let miniMap = new L.Control.MiniMap(L.tileLayer.provider("OpenStreetMap.DE"), { toggleDisplay: true, minimized: true }).addTo(map);
+
 // Rainviewer Plugin
-L.control.rainviewer({ 
+L.control.rainviewer({
     position: 'bottomleft',
     nextButtonText: '>',
     playStopButtonText: 'Play/Stop',
@@ -59,18 +65,26 @@ function writeHuettenLayer(jsondata) {
         onEachFeature: function (feature, layer) {
             let prop = feature.properties;
             layer.bindPopup(`
-            <h1>${prop.name}, ${ele|| " "} m ü.A. </h1><ul>
-                <li>Öffnungszeiten: ${prop.opening_ho || "-"} </li>
-                <li>Website: ${prop.contact_we || "-"} </li>
-                <li>Telefon: ${prop.phone|| "-"}</li>
+            <h1>${prop.name}, ${prop.ele || ""} m ü.A. </h1><ul>
+                <li>Öffnungszeiten: ${prop.opening_ho || "-"}</li>
+                <li>Website: ${prop.contact_we || "-"}</li>
+                <li>Telefon: ${prop.phone || "-"}</li>
                 <li>Email: ${prop.email || "-"}</li>
-            </ul></>
+            </ul>
         `);
         }
-    }).addTo(themaLayer.huetten)
+    }).addTo(themaLayer.huetten);
 }
-writeHuettenLayer(data/Hütten_Tirol.geojson)
-// Haltestellen (Bus)
+//Funktion ausführen, indem JSON gefetched wird
+fetch("data/Hütten_Tirol.geojson")
+    .then(response => response.json())
+    .then(jsondata => {
+        writeHuettenLayer(jsondata);
+    })
+    .catch(error => {
+        console.error("Error fetching GeoJSON data:", error);
+    });
+//Haltestellen (Bus)
 function writeBusLayer(jsondata) {
     L.geoJSON(jsondata, {
         pointToLayer: function (feature, latlng) {
@@ -85,13 +99,22 @@ function writeBusLayer(jsondata) {
         onEachFeature: function (feature, layer) {
             let prop = feature.properties;
             layer.bindPopup(`
-            <h1>${prop.name}</h1>
+            <h1>${prop.name}
         `);
         }
-    }).addTo(themaLayer.stops_bus)
+    }).addTo(themaLayer.stops_bus);
 }
-writeBusLayer(data/bus_stop.geojson);
+//Funktion ausführen, indem JSON gefetched wird
+fetch("data/bus_stop.geojson")
+    .then(response => response.json())
+    .then(jsondata => {
+        writeBusLayer(jsondata);
+    })
+    .catch(error => {
+        console.error("Error fetching GeoJSON data:", error);
+    });
 // Haltestellen (Tram)
+
 function writeTramLayer(jsondata) {
     L.geoJSON(jsondata, {
         pointToLayer: function (feature, latlng) {
@@ -106,9 +129,17 @@ function writeTramLayer(jsondata) {
         onEachFeature: function (feature, layer) {
             let prop = feature.properties;
             layer.bindPopup(`
-            <h1>${prop.name}</h1>
+            <h1>${prop.name}
         `);
         }
-    }).addTo(themaLayer.stops_tram)
+    }).addTo(themaLayer.stops_tram);
 }
-writeTramLayer(data/tram_stop.geojson);
+//Funktion ausführen, indem JSON gefetched wird
+fetch("data/tram_stop.geojson")
+    .then(response => response.json())
+    .then(jsondata => {
+        writeTramLayer(jsondata);
+    })
+    .catch(error => {
+        console.error("Error fetching GeoJSON data:", error);
+    });
